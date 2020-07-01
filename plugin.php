be215cb6000887d904dfaf1f34185d4ba227af8b
Anton Ukhanev <xedin.unknown@gmail.com>
2020-07-01 23:53:06 +0200

    Add main plugin file

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * My WordPress plugin.
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name: My WordPress Plugin
+ * Description: A project skeleton useful for starting a new WordPress plugin
+ * Version: 0.1.0-alpha1
+ * Author: Me
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: plugin
+ * Domain Path: /languages
+ */
