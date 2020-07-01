00e189066a38e657daecc6a23c29ba8029706d75
Anton Ukhanev <xedin.unknown@gmail.com>
2020-07-02 01:46:01 +0200

    Add comment to entrypoint script

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -14,6 +14,8 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
   wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
+
+  # Custom setup instructions
 fi
 
 exec "$@"
