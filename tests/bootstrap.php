a56f66d439d66e7769d11eb1f398150235d94dfc
Anton Ukhanev <xedin.unknown@gmail.com>
2020-07-07 17:20:37 +0200

    New `ROOT_DIR` constant now available in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 (function (string $baseFile) {
     $baseDir = dirname($baseFile);
     $rootDir = dirname($baseDir);
+    define('ROOT_DIR', $rootDir);
 
     error_reporting(E_ALL | E_STRICT);
 
